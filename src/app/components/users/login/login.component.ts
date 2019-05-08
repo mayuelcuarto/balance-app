b4fe53f68a644baceae5f63ed31d9846cf2e9d0c
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-08 10:38:12 -0500

    Regerando componentes de login

---
+++ src/app/components/users/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+}
