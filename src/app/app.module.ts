8f08461fd58e9505491a27098347da79818c386f
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-08 10:38:48 -0500

    Importando forms y reactiveforms modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ConceptosComponent } from './components/conceptos/conceptos.component';
-import { LoginComponent } from './components/home/login/login.component';
-import { RegisterComponent } from './components/home/register/register.component';
-import { ProfileComponent } from './components/home/profile/profile.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { ProfileComponent } from './components/users/profile/profile.component';
 
 import { MaterialModule } from './material.module';
 import { MainMenuComponent } from './main-menu/main-menu.component';
@@ -17,15 +20,17 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
   declarations: [
     AppComponent,
     ConceptosComponent,
+    HomeComponent,
+    MainMenuComponent,
     LoginComponent,
     RegisterComponent,
-    ProfileComponent,
-    HomeComponent,
-    MainMenuComponent
+    ProfileComponent
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     MaterialModule
   ],
