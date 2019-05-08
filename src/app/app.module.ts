f6876f82c2ef72d5a309311aa4921cb96a4422f2
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-08 08:44:25 -0500

    Generando nuevos componentes
    -  Generando componentes para: conceptos, login, register y profile.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConceptosComponent } from './components/conceptos/conceptos.component';
+import { LoginComponent } from './components/home/login/login.component';
+import { RegisterComponent } from './components/home/register/register.component';
+import { ProfileComponent } from './components/home/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConceptosComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
