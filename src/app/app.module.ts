c1831ca0b307f39c2086f7626f1ab707ab5477ba
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-08 09:25:33 -0500

    Cambios para optimización de diseño de main-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
     MainMenuComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     MaterialModule
