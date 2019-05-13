6039964e7c41b1f0810e98dc9b8572113b78197b
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-13 01:23:37 -0500

    Cambios en login_ts
    - Importación de modulos necesarios.
    - Inicialización de propiedades de router y auth en constructor.
    - Creación de métodos para logeo.

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +12,59 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   hide = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor() { }
+  constructor(
+    public afAuth: AngularFireAuth, 
+    private router: Router,
+    private authService: AuthService
+    ) { }
+
+  formLogin = new FormGroup ({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('')
+  });
 
   ngOnInit() {
   }
 
+  onLogin(): void{
+    console.log('email', this.formLogin.value.email);
+    console.log('password', this.formLogin.value.password);
+    this.authService.loginEmailUser(this.formLogin.value.email, this.formLogin.value.password)
+    .then((res) => {
+      this.onLoginRedirect();
+    }).catch(err => this.onCatchError(err));    
+  }
+
+  onLoginGoogle(): void{
+    this.authService.loginGoogleUser()
+    .then((res) => {
+      this.onLoginRedirect();
+    }).catch(err => this.onCatchError(err));    
+  }
+
+  onLoginFacebook(): void{
+    this.authService.loginFacebookUser()
+    .then((res) => {
+      this.onLoginRedirect();
+    }).catch(err => this.onCatchError(err));    
+  }
+
+  onLogout(): void{
+    this.authService.logoutUser();
+  }
+
+  onLoginRedirect(): void{
+    this.router.navigate(['conceptos/']);
+  }
+
+  onCatchError(err): void{
+    console.log('err', err.message);
+  }
+
   getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
+    return this.formLogin.get('email').hasError('required') ? 'Debes ingresar un valor' :
+        this.formLogin.get('email').hasError('email') ? 'Email inválido' :
             '';
   }
 
