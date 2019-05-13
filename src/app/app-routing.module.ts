02bad75154f069406eeb70c165896294491b1326
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-13 01:14:57 -0500

    Cambios en routing_module
    - Añadiendo rutas para mantenimiento de conceptos y categorías.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { ConceptosComponent } from './components/conceptos/conceptos.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
 
 const routes: Routes = [
-	{ path: '', component: HomeComponent }
+	{ path: '', component: HomeComponent },
+	{ path: 'conceptos', component: ConceptosComponent },
+	{ path: 'categorias', component: CategoriasComponent },
 ];
 
 @NgModule({
