ce22eb97db9be3297894463f09f645a14aa93c8b
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-13 01:25:20 -0500

    Cambios en main-menu_ts
    - Importación de servicio de auth y modulo de AngularFireAuth.
    - Implementación de métodos para verificar el login.

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -3,6 +3,9 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 @Component({
   selector: 'main-menu',
   templateUrl: './main-menu.component.html',
@@ -15,6 +18,31 @@ export class MainMenuComponent {
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+  	private breakpointObserver: BreakpointObserver,
+  	private authService: AuthService,
+  	private afsAuth: AngularFireAuth
+  	) {}
 
+  public isLogged: boolean = false;
+
+  getCurrentUser(){
+  	this.authService.isAuth().subscribe(auth => {
+  		if(auth){
+  			console.log('user logged');
+  			this.isLogged = true;
+  		}else{
+  			console.log('user not logged');
+  			this.isLogged = false;
+  		}
+  	})
+  }
+
+  ngOnInit() {
+  	this.getCurrentUser();
+  }
+
+  onLogout(){
+  	this.afsAuth.auth.signOut();
+  }
 }
