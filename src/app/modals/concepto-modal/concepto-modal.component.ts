204b895e1a57f14b16ae58c66f6a8d34666d98d4
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-13 01:26:10 -0500

    Creación de modal concepto

---
+++ src/app/modals/concepto-modal/concepto-modal.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+import { ConceptoInterface } from '../../models/concepto';
+import { TipoInterface } from '../../models/tipo';
+import { CategoriaInterface } from '../../models/categoria';
+
+import { ConceptosService } from '../../services/conceptos.service';
+import { CategoriasService } from '../../services/categorias.service';
+
+import { NgForm, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-concepto-modal',
+  templateUrl: './concepto-modal.component.html',
+  styleUrls: ['./concepto-modal.component.css']
+})
+export class ConceptoModalComponent implements OnInit {
+  constructor(    
+    public dialogRef: MatDialogRef<ConceptoModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public conceptoService: ConceptosService,
+    public categoriaService:  CategoriasService
+    ) {}
+
+  public tipos: TipoInterface[] = [
+    {id: 'ingreso', name: 'Ingresos'},
+    {id: 'egreso', name: 'Egresos'}
+  ];
+
+  public categorias: CategoriaInterface[] = [];
+
+  selectedTipo = 'Ingreso';
+  fecha = null;
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {
+    this.getCategoriasByTipo('ingreso');
+    this.fecha = new FormControl(this.data.selectedConcepto.date.toDate());
+  }
+
+  onSaveConcepto(conceptoForm: NgForm): void{
+    if(this.data.selectedConcepto.id == null){
+      // New
+      console.log('New', conceptoForm.value);
+      if(this.data.selectedConcepto.description != null 
+        && this.data.selectedConcepto.mount != null
+        && this.data.selectedConcepto.date != null){
+        conceptoForm.value.date = this.fecha.value;
+        this.conceptoService.createConcepto(conceptoForm.value);
+      }
+    }else{
+      // Edit
+      console.log('Update', conceptoForm.value);
+      if(this.data.selectedConcepto.description != null 
+        && this.data.selectedConcepto.mount != null
+        && this.data.selectedConcepto.date != null){
+        conceptoForm.value.date = this.fecha.value;
+        this.conceptoService.updateConcepto(conceptoForm.value);
+      }
+    }
+    //console.log('Else', conceptoForm.value);
+    conceptoForm.resetForm();
+    this.dialogRef.close();
+  }
+
+  getAllCategorias(): void{
+    this.categoriaService.getCategorias().subscribe(res => {
+      this.categorias = res;
+    });
+  }
+
+  getCategoriasByTipo(tipo: string): void{
+    this.categoriaService.getCategoriasByTipo(tipo).subscribe(res => {
+      this.categorias = res;
+    });
+  }
+
+}
