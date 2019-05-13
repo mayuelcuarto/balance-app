c055ea0924c05aa25fc19e4dc19e5c56991b21e7
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-13 01:19:11 -0500

    Cambios en conceptos_ts
    - Importación de modulos necesarios.
    - Creación de métodos de dialog.
    - Creación de métodos para envio de datos a dialog.

--- src/app/components/conceptos/conceptos.component.ts
+++ src/app/components/conceptos/conceptos.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { MatSort, MatTableDataSource, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { ConceptosService } from '../../services/conceptos.service';
+import { ConceptoInterface } from '../../models/concepto';
+import { ConceptoModalComponent } from '../../modals/concepto-modal/concepto-modal.component';
 
 @Component({
   selector: 'app-conceptos',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConceptosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private conceptoService: ConceptosService, public dialog: MatDialog) { }
+  displayedColumns: string[] = ['type','category','description', 'mount', 'date', 'actions'];
+  dataSource = new MatTableDataSource();
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ConceptoModalComponent, {
+      width: '250px',
+      data: {selectedConcepto: this.conceptoService.selectedConcepto,
+        }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      
+    });
+  }
+
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
+  	this.getAllConceptos();
+  }
+
+  ngAfterViewInit(){
+    this.dataSource.sort = this.sort;
+  }
+
+  getAllConceptos(){
+    this.conceptoService.getConceptos().subscribe(res => {
+       this.dataSource.data = res;
+    });
+
+  }
+
+  onChangeStatus(concepto: ConceptoInterface){
+    this.conceptoService.updateConcepto(concepto);
+  }
+
+  onPreUpdateConcepto(concepto: ConceptoInterface){
+    this.conceptoService.selectedConcepto = Object.assign({}, concepto);
+    this.openDialog();
+  }
+
+  onDeleteConcepto(idConcepto: string){
+    const confirmacion = confirm('Are you sure?');
+    if(confirmacion){
+      this.conceptoService.deleteConcepto(idConcepto);
+    }
   }
 
 }
