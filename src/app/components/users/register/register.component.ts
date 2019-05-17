ba272d6eca08d5b78bdf37f0e4de5012713e8eb7
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-16 21:49:02 -0500

    Cambios en register_ts
    - Importación de ElementRef, ViewChild, FormControl, FormGroup, Validators, AuthService, Router, AngularFirestore, finalize y Observable.
    - Con @ViewChild se captura el valor de inpuImageUser.
    - Creación de variables uploadPercent y urlImage.
    - Creación de propiedades router, authService y storage como privadas en el constructor.
    - Creación de método onUpload().
    - Creación de método onAddUser().
    - Creación de método onLoginGoogle().
    - Creación de método onLoginFacebook().
    - Creación de método on RegisterRedirect().
    - Creación de método onCatchError().

--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +12,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('imageUser') inputImageUser: ElementRef;
+  hide = true;
 
-  constructor() { }
+  uploadPercent: Observable<number>;
+  urlImage: Observable<string>;
+  constructor(
+  	private router: Router, 
+  	private authService: AuthService, 
+  	private storage: AngularFireStorage
+  	) { }
+
+  formRegister = new FormGroup ({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('')
+  });
 
   ngOnInit() {
   }
 
+  onUpload(e){
+    //console.log('subir',e.target.files[0]);
+    const id = Math.random().toString(36).substr(2);
+    const file = e.target.files[0];
+    const filePath = `uploads / profile_${ id }`;
+    const ref = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    this.uploadPercent = task.percentageChanges();
+    task.snapshotChanges().pipe(finalize(() => this.urlImage = ref.getDownloadURL())).subscribe();
+  }
+
+  onAddUser(){
+  	this.authService.registerUser(this.formRegister.value.email, this.formRegister.value.password)
+  	.then((res) => {
+      this.authService.isAuth().subscribe(user => {
+        if(user){
+          user.updateProfile({
+            displayName: '',
+            photoURL: this.inputImageUser.nativeElement.value
+          }).then((res) => {
+            this.onRegisterRedirect();
+          }).catch(err => this.onCatchError(err));
+        }
+      })
+  	}).catch(err => this.onCatchError(err));
+  }
+
+  onLoginGoogle(): void{
+    this.authService.loginGoogleUser()
+    .then((res) => {
+      this.onRegisterRedirect();
+    }).catch(err => this.onCatchError(err));  	
+  }
+
+  onLoginFacebook(): void{
+  	this.authService.loginFacebookUser()
+    .then((res) => {
+      this.onRegisterRedirect();
+    }).catch(err => this.onCatchError(err));    
+  }
+
+  onRegisterRedirect(): void{
+    this.router.navigate(['conceptos/']);
+  }
+
+  onCatchError(err): void{
+    console.log('err', err.message);
+  }
 }
