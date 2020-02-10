2e17cf33e686206a19484e4b21cb70735c60c0a2
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2020-02-09 19:18:58 -0500

    Añadiendo filtro en material table

--- src/app/components/conceptos/conceptos.component.ts
+++ src/app/components/conceptos/conceptos.component.ts
@@ -42,6 +42,12 @@ export class ConceptosComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
   ngOnInit() {
     this.getCurrentUser();
   }
