23eee2e6667a7d87c76d6e56acfd5bca41140cd8
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2022-07-09 15:21:16 -0500

    Ignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,8 @@ export const environment = {
     databaseURL: "https://balanceapp-31e4c.firebaseio.com",
     projectId: "balanceapp-31e4c",
     storageBucket: "balanceapp-31e4c.appspot.com",
-    messagingSenderId: "762640607428"
+    messagingSenderId: "762640607428",
+    appId: "1:762640607428:web:a2d62cb3c4053fad",
+    measurementId: "G-NR0YZ6DV62"
   }
 };
